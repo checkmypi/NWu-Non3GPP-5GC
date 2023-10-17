1b95883e2fc78c78025f15ed61ab067bdac067ee
Check My Pi <checkmypi@github.com>
2023-10-17 10:00:06 +0200

    Add ifconfig and priviledged mode

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PARENT_DIR=$(dirname ${SCRIPT_DIR})
 PARENT_DIR=$(dirname ${PARENT_DIR})
 
-docker run -v ${PARENT_DIR}:/opt -w /opt/NWu-Non3GPP-5GC --cap-add NET_ADMIN -it n3iwf-sim 
+docker run -v ${PARENT_DIR}:/opt -w /opt/NWu-Non3GPP-5GC --cap-add NET_ADMIN --privileged -it n3iwf-sim 
